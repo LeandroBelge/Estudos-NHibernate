94b3f6c83f8be16ad35a976a5241cc414f23670a
Leandro Belge da Cunha <leandrobelge@gmail.com>
2016-07-07 16:10:04 -0300

    Terceiro post terminado.

--- EstudandoNHibernate/EstudandoNHibernate/Program.cs
+++ EstudandoNHibernate/EstudandoNHibernate/Program.cs
@@ -1,4 +1,5 @@
-﻿using EstudandoNHibernate.Infra;
+﻿using EstudandoNHibernate.Domains;
+using EstudandoNHibernate.Infra;
 using NHibernate;
 using NHibernate.Cfg;
 using NHibernate.Tool.hbm2ddl;
@@ -15,19 +16,41 @@ namespace EstudandoNHibernate
     {
         static void Main(string[] args)
         {
+            //Recupera minha configuração do NHibernate
+            Configuration configuracao = new Configuration();
+            configuracao.Configure();
+            configuracao.AddAssembly(Assembly.GetExecutingAssembly());
+            
+            //Instanciar e abrir minha conexão
+            ISession session = configuracao.BuildSessionFactory().OpenSession();
 
-            NHibernateHelper helper = new NHibernateHelper();
-            Configuration configuracao = helper.configuracao();
-            try
-            {
-                new SchemaUpdate(configuracao).Execute(true, true);
-                Console.WriteLine("Entidade criada no BD");
-            }
-            catch
-            {
-                Console.WriteLine("Falha ao criar a entidade no BD");
-            }
-            Console.ReadLine();
+            //iniciando uma transação que irá gravar minha alteração no BD.
+            ITransaction transacao = session.BeginTransaction();
+
+            //Instanciar uma pessoa e atribuir um nome a ela.
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "Primeira pessoa";
+
+            //Pedir a minha session para guardar a pessoa no BD.
+            session.SaveOrUpdate(pessoa);
+
+            //Confirmar minha transação.
+            transacao.Commit();
+
+            //Recuperando uma entidade pelo ID
+            session.Get<Pessoa>(pessoa.Id);
+
+            //Iniciando nova transação
+            ITransaction novaTransacao = session.BeginTransaction();
+
+            //Removendo minha entidade do BD.
+            session.Delete(pessoa);
+
+            //Confirmando transação
+            novaTransacao.Commit();
+
+            //Fechar minha conexão
+            session.Close();
         }
     }
 }
